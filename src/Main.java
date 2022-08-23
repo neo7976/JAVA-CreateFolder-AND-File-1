c88c51d13bab4c11731538e213024a320404c6a1
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-23 18:25:30 +0300

    Шаблон написания папок

--- src/Main.java
+++ src/Main.java
@@ -7,38 +7,38 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
 
     public static final String WAY = "E:/IT/Java/0. Обучение Java/1. Нетология/Java-разработчик с нуля" + "/";
-    public static final String PATHNAME = "5465555465465" + "/";
+    public static final String PATHNAME = "5. Шаблоны проектирования" + "/";
 
     public static final String PARHNAME_STANDART1 = "/" + "Homework";
     public static final String PARHNAME_STANDART2 = "/" + "Video";
-    public static final String PARHNAME_STANDART3 = "/" +"Тест";
+    public static final String PARHNAME_STANDART3 = "/" + "Тест";
 
     public static void main(String[] args) {
         File games = new File(WAY + PATHNAME);
         folderCreation(games);
 
-        String folder1 = "null1";
-        String folder2 = "null2";
-        String folder3 = "null3";
-        String folder4 = "null4";
-        String folder5 = "null5";
-        String folder6 = "null6";
-        String folder7 = "null7";
-        String folder8 = "null8";
-        String folder9 = "null9";
-        String folder10 = "null10";
+        String folder1 = "1. " + "Порождающие шаблоны (Builder, Singleton, Factory Method, Abstract Factory, Prototype)";
+        String folder2 = "2. " + "Структурные шаблоны (Proxy, Decorator, Adapter)";
+        String folder3 = "3. " + "Поведенческие шаблоны (Command, Iterator, Observer, Chain of Responsibility)";
+        String folder4 = "4. " + "Magics, DRY, SOLID";
+//        String folder5 = "5. " + "null5";
+//        String folder6 = "6. " + "null6";
+//        String folder7 = "7. " + "null7";
+//        String folder8 = "8. " + "null8";
+//        String folder9 = "9. " + "null9";
+//        String folder10 = "10. " + "null10";
 
         //1
-        createFolderOrFile(WAY + PATHNAME, folder1
+        createFolder(WAY + PATHNAME, folder1
                 , folder2
                 , folder3
                 , folder4
-                , folder5
-                , folder6
-                , folder7
-                , folder8
-                , folder9
-                , folder10
+//                , folder5
+//                , folder6
+//                , folder7
+//                , folder8
+//                , folder9
+//                , folder10
         );
 
         //2
@@ -46,12 +46,12 @@ public class Main {
         createFolderOrFile(WAY + PATHNAME + folder2, PARHNAME_STANDART1, PARHNAME_STANDART2, PARHNAME_STANDART3);
         createFolderOrFile(WAY + PATHNAME + folder3, PARHNAME_STANDART1, PARHNAME_STANDART2, PARHNAME_STANDART3);
         createFolderOrFile(WAY + PATHNAME + folder4, PARHNAME_STANDART1, PARHNAME_STANDART2, PARHNAME_STANDART3);
-        createFolderOrFile(WAY + PATHNAME + folder5, PARHNAME_STANDART1, PARHNAME_STANDART2, PARHNAME_STANDART3);
-        createFolderOrFile(WAY + PATHNAME + folder6, PARHNAME_STANDART1, PARHNAME_STANDART2, PARHNAME_STANDART3);
-        createFolderOrFile(WAY + PATHNAME + folder7, PARHNAME_STANDART1, PARHNAME_STANDART2, PARHNAME_STANDART3);
-        createFolderOrFile(WAY + PATHNAME + folder8, PARHNAME_STANDART1, PARHNAME_STANDART2, PARHNAME_STANDART3);
-        createFolderOrFile(WAY + PATHNAME + folder9, PARHNAME_STANDART1, PARHNAME_STANDART2, PARHNAME_STANDART3);
-        createFolderOrFile(WAY + PATHNAME + folder10, PARHNAME_STANDART1, PARHNAME_STANDART2, PARHNAME_STANDART3);
+//        createFolderOrFile(WAY + PATHNAME + folder5, PARHNAME_STANDART1, PARHNAME_STANDART2, PARHNAME_STANDART3);
+//        createFolderOrFile(WAY + PATHNAME + folder6, PARHNAME_STANDART1, PARHNAME_STANDART2, PARHNAME_STANDART3);
+//        createFolderOrFile(WAY + PATHNAME + folder7, PARHNAME_STANDART1, PARHNAME_STANDART2, PARHNAME_STANDART3);
+//        createFolderOrFile(WAY + PATHNAME + folder8, PARHNAME_STANDART1, PARHNAME_STANDART2, PARHNAME_STANDART3);
+//        createFolderOrFile(WAY + PATHNAME + folder9, PARHNAME_STANDART1, PARHNAME_STANDART2, PARHNAME_STANDART3);
+//        createFolderOrFile(WAY + PATHNAME + folder10, PARHNAME_STANDART1, PARHNAME_STANDART2, PARHNAME_STANDART3);
 
 //        //3
 //        createFolderOrFile(WAY + PATHNAME+ "/src/main", "Main.java", "Utils.java");
@@ -94,6 +94,13 @@ public class Main {
                 fileCreation(file1);
             else
                 folderCreation(file1);
+        }
+    }
+
+    public static void createFolder(String file, String... folder) {
+        for (String s : folder) {
+            File file1 = new File(file, s);
+            folderCreation(file1);
         }
     }
 }
